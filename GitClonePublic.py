39a6da311533fe35dc3de31d98df30dc52aba4e3
Daedd <r.macmillan863@gmail.com>
2023-04-10 17:34:44 -0400

    initial state

--- GitClonePublic.py
+++ GitClonePublic.py
@@ -3,8 +3,6 @@ from github import Github
 
 g = Github()
 
-public = []
-
 ORG = input("USER/ORG: ")
 
 user = g.get_user(ORG)
@@ -12,6 +10,5 @@ repos = user.get_repos()
 
 for repo in user.get_repos():
     if repo.private is False:
-        public.append(repo.name)
         print("Cloning: ", repo.full_name)
         Repo.clone_from(f"https://github.com/{ORG}/{repo.name}", f"/temp/CLONED/{repo.full_name}")
